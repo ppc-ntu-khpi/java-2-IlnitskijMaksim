64b072e3a9e10717cf8c32e12aa808f6daa3c066
Ільницький Максим <112692170+IlnitskijMaksim@users.noreply.github.com>
2023-02-18 17:48:35 +0200

    Update Exercise.java

--- src/Exercise.java
+++ src/Exercise.java
@@ -1,7 +1,22 @@
-//package domain;
+package domain;
 
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+
+
+    public static String Calculate(String d){
+        //Цикл додає числа рядка d, поки його довжина не досягне 1 символа
+           while (d.length() > 1) {
+            int sum = 0;
+            for (char c : d.toCharArray()) {
+                //Перевіряє, чи є символ цифрою і віднімає символ 0, щоб отримати числове значення
+                if (Character.isDigit(c)) {
+                    sum += (c - '0');
+                }
+            }
+            //Призначення нового значення для радка d
+            d = String.valueOf(sum);
+        }
+        //Повертає вже обчислений рядок d
+        return d;
     }
 }
